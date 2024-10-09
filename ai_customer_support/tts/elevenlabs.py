0bbc3f0fe47ee679b609540b1f40261d55e83784
Murugan Pandian <murugan@telecmi.com>
2024-10-09 16:22:24 +0530

    TTS fix

--- ai_customer_support/tts/elevenlabs.py
+++ ai_customer_support/tts/elevenlabs.py
@@ -6,7 +6,7 @@ import json
 from piopiy import StreamAction
 
 
-ELEVEN_URL = "https://api.elevenlabs.io/v1/text-to-speech/stream?output_format=mp3_22050_32"
+ELEVEN_URL = "https://api.elevenlabs.io/v1/text-to-speech/EXAVITQu4vr4xnSDxMaL/stream?output_format=mp3_22050_32"
 ELEVEN_API_KEY = os.getenv("EL_API_KEY")
 
 
@@ -14,11 +14,11 @@ def get_speech(text):
     payload = {
      "text": text,
      "voice_id": "EXAVITQu4vr4xnSDxMaL",
-     "voice_settings": {"similarity_boost": 0.85}
+     "voice_settings": {"stability":0.1,"similarity_boost": 0.85}
      }
 
  
-
+    print(ELEVEN_API_KEY)
     headers = {
       "xi-api-key": ELEVEN_API_KEY,
       "content-type": "application/json"
@@ -28,7 +28,7 @@ def get_speech(text):
     TARGET_CHUNK_SIZE = 50 * 1024 
    
     response = requests.post(ELEVEN_URL, headers=headers, json=payload, stream=True)
-     
+    print(response)
     if response.status_code == 200:
       
         accumulated_chunk = b""
